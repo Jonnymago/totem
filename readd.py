618c427bcb498e867686fe3761d0cd6f2a357746
AI Studio <priologiovanni82@gmail.com>
2026-09-05 11:53:22 +0000

    chore: remove temporary scripts

--- readd.py
+++
@@ -1,7 +0,0 @@
-with open('frontend/src/utils/i18n.ts', 'r') as f:
-    content = f.read()
-
-content = content.replace("Object.assign(LITERAL_TRANSLATIONS.en, {", "Object.assign(LITERAL_TRANSLATIONS.en, {\n  'Gestione catalogo e personalizzazioni': 'Catalog and customization management',\n  'Nuovo': 'New',")
-
-with open('frontend/src/utils/i18n.ts', 'w') as f:
-    f.write(content)
